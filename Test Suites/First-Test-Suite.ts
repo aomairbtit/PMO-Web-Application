59c09c5ad877bb08a0c3c9eba4d468e893ba054f
HP <HP@10.152.69.144>
2019-04-02 10:22:07 +0300

    changes

---
+++ Test Suites/First-Test-Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>First-Test-Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>1ef5fdb1-59a8-423f-875f-1d18c15693ed</testSuiteGuid>
+   <testCaseLink>
+      <guid>8da77740-ba4e-4177-9ddd-6f1a6122a919</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/First-Test-Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
