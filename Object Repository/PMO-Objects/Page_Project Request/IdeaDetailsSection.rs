2d00a0c25640e507d5633ad97408faa18e35eae0
HP <HP@HP45G5>
2019-04-21 10:22:05 +0300

    add object

---
+++ Object Repository/PMO-Objects/Page_Project Request/IdeaDetailsSection.rs
@@ -0,0 +1,207 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>IdeaDetailsSection</name>
+   <tag></tag>
+   <elementGuidId>5c420051-510f-47f1-bff9-b23fb09d93a3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='تفاصيل فكرة'])[2]/following::div[12]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-block</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                            المعلومات الأساسية
+                            
+
+                                
+                                    
+
+
+                                        
+
+                                            
+                                                
+                                                    
+                                                         اسم الفكرة
+                                                        MA1
+
+                                                    
+                                                
+
+                                                
+                                                    تاريخ البدء المتوقع
+                                                    04/23/2019
+
+
+                                                
+                                            
+                                            
+                                                
+                                                    
+                                                        تاريخ الإنتهاء المتوقع
+                                                         04/28/2019
+
+
+                                                    
+                                                
+
+                                                
+                                                    مدة الفكرة (يوم)
+                                                     6
+
+
+                                                
+
+                                            
+                                            
+                                                
+                                                    
+                                                        الهدف من الفكرة
+                                                        الهدف من الفكرة
+
+
+                                                    
+                                                
+
+                                                
+                                                    الميزانية التقديرية (ريال)
+                                                    5000.00
+
+
+
+                                                
+                                            
+                                            
+                                                
+                                                    
+                                                        ملاحظات
+                                                        
+
+                                                    
+                                                
+
+                                                
+                                                    المحفظة
+                                                    محفظة 1
+
+
+
+                                                
+                                            
+                                            
+                                                
+                                                    
+                                                        هل الفكرة مرتبطة بهدف استراتيجي
+                                                         نعم
+
+
+                                                    
+                                                
+
+                                                
+                                                    
+                                                        البرنامج
+                                                        
+
+
+                                                    
+                                                
+
+
+                                            
+
+                                            
+
+                                                
+                                                    تاريخ التنفيذ المتوقع
+                                                        
+                                                            08/16/1440
+                                                            (04/21/2019)
+                                                        
+                                                
+
+                                                
+                                                    
+                                                        الأولوية
+                                                        3
+                                                    
+                                                
+                                            
+                                                
+                                                    
+                                                        
+                                                            الأهداف
+                                                            
+
+
+                                                        
+                                                    
+
+                                                    
+                                                        مبادرات التحول الوطني
+                                                        إطلاق المعايرة القانونية
+
+                                                    
+                                                
+
+
+
+
+
+
+
+
+                                        
+                                    
+                                
+                            
+                        </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/section[@class=&quot;content&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-sm-10 col-md-10 col-lg-10 pl-0&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-lg-12 p-0&quot;]/div[@class=&quot;col-lg-12&quot;]/div[@class=&quot;projectsbackground pt-2 pb-1&quot;]/div[@class=&quot;panel&quot;]/div[@class=&quot;col-lg-12&quot;]/div[1]/div[@class=&quot;row&quot;]/div[@class=&quot;col-sm-12 col-md-9 col-lg-9&quot;]/div[@class=&quot;card wow fadeInUp&quot;]/div[@class=&quot;card-block&quot;]/div[@class=&quot;card&quot;]/div[@class=&quot;card-block&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='تفاصيل فكرة'])[2]/following::div[12]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='قائمة الأفكار'])[1]/following::div[12]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <value>//div[2]/div/div[2]/div/div/div/div/div/div/div/div/div/div/div</value>
+   </webElementXpaths>
+</WebElementEntity>
